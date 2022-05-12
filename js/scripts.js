aadba038020fa6546fa53fa3a4857eb6026a7707
Joshua Vining <joshuavining86@gmail.com>
2022-05-12 17:11:15 -0400

    re-commit

--- js/scripts.js
+++ js/scripts.js
@@ -8,34 +8,22 @@ $(function($) {
     $(window).scroll(function() {
 	var navbrand = $(".navbar-brand");
     if ($(this).scrollTop() >= 180) {  
-    	navbrand.removeClass('fa-3x');
+    	navbrand.removeClass('fa-2x');
 	} else
-		navbrand.addClass('fa-3x');
+		navbrand.addClass('fa-2x');
  	});
 });
-
-// Enables Smooth Scrolling
-$('a[href*="#"]')
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function(event) {
-	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname)	{
-	var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-		event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 500, function() {
-		  var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) {
-          return false;
-        } else {
-          $target.attr('tabindex','-1');
-          $target.focus(); 
-        };
-      });
-    }
-  }
+ 
+// Enables Scroll with Offsets
+$('.scroll').on('click',function(e) {
+	e.preventDefault();
+	var offset = 0;
+	var target = this.hash;
+	if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+	$('html, body').stop().animate({
+		'scrollTop': $(target).offset().top - offset
+	}, 500, 'swing', function() {
+		// window.location.hash = target;
+	});
 });
+ 
